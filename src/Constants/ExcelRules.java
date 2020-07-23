cf487ed4f7faa97a919c4eaa997064ba7f41c736
KostasThan <36800742+KostasThan@users.noreply.github.com>
2020-07-23 20:57:18 +0300

    Testing Git

--- src/Constants/ExcelRules.java
+++ src/Constants/ExcelRules.java
@@ -3,5 +3,5 @@ package Constants;
 public class ExcelRules {
 	private ExcelRules() {}
 	public static final short MAX_WORKING_HOURS=24;
-	public static final short MAX_COLUMN = 10;
+	public static final short MAX_COLUMN = 10;  
 }
